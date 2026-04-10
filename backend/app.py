40846222f538887d4b031edd7ec23c953c339bd1
zerotozenith01-rgb <zerotozenith01@gmail.com>
2026-04-11 01:25:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import os
 from routes.medicine_routes import medicine_bp
 from routes.pharmacy_routes import pharmacy_bp
 from routes.review_routes import review_bp
@@ -37,4 +37,5 @@ def home():
     return {"message": "SaltSense Backend Running"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
